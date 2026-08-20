c32e01cce588eeb65e586e8510800b11d010c267
tim-s-ccs <Timothy.South@communities.gov.uk>
2026-08-20 17:12:17 +0100

    fix: update browser test for change to card HTML

--- browser_tests/pages/home_page.py
+++ browser_tests/pages/home_page.py
@@ -2,8 +2,5 @@ from .share_page import SharePage
 
 
 class HomePage(SharePage):
-    # TODO: Replace when we have the card updated HFURB-3979
     def click_on_card(self, card_text):
-        self.page.get_by_role("heading", level=3, name=card_text).locator(
-            "xpath=../../../../a"
-        ).click()
+        self.page.get_by_role("heading", level=3, name=card_text).click()
